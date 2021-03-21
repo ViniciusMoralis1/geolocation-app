e2000f9f1171e1c401dc27b62ce1606335320033
Vinicius Moralis <vinicius_moralis@hotmail.com>
2021-03-21 15:18:03 -0300

    atualizando localizacoes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,49 +7,54 @@ import Icon from 'react-native-vector-icons/Feather';
 import styles from './styles';
 
 export default function Home() {
-  const [latitude, setLatitude] = useState(-23.583776);
-  const [longitude, setLongitude] = useState(-46.588805);
+  const [currentRegion, setCurrentRegion] = useState(null);
   const [locations, setLocations] = useState([]);
 
-  const requestGeolocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: "Preciso da sua permissão",
-          message:
-            "Sua permissão é necessária para podermos acessar sua localização",
-          buttonNeutral: "Pergunte depois",
-          buttonNegative: "Cancelar",
-          buttonPositive: "OK"
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("Podemos usar a localização");
-        Local.getCurrentPosition((pos) => {
-          setLatitude(pos.coords.latitude);
-          setLongitude(pos.coords.longitude);
-          console.log("latitude: " + latitude + " longitude: " + longitude)
-        }), 
-        (err) => {
-          console.log("Erro: " + err)
-        },
-        { 
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 1000
-        }
-      } else {
-        console.log("Não podemos usar a localização");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
-
   useEffect(() => {
+    async function requestGeolocationPermission() {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Preciso da sua permissão",
+            message:
+              "Sua permissão é necessária para podermos acessar sua localização",
+            buttonNeutral: "Pergunte depois",
+            buttonNegative: "Cancelar",
+            buttonPositive: "OK"
+          }
+        );
+        if(granted === PermissionsAndroid.RESULTS.GRANTED) {
+          Local.getCurrentPosition((pos) => {
+            const { latitude, longitude } = pos.coords;
+            setCurrentRegion({latitude, longitude, latitudeDelta: 1, longitudeDelta: 1})
+            console.log("latitude: " + latitude + " longitude: " + longitude)
+          }), 
+          (err) => {
+            console.log("Erro: " + err)
+          },
+          { 
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 1000
+          }
+        } else {
+          console.log("Não podemos usar a localização");
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
     requestGeolocationPermission();
   }, []);
+
+  function handleRegionChanged(region){
+    setCurrentRegion(region);
+  }
+
+  if(!currentRegion){
+    return null;
+  }
 
   function addLocation(){
     console.log('add location')
@@ -61,16 +66,7 @@ export default function Home() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 1.1,
-          longitudeDelta: 1.1
-        }}
-        zoomEnabled = {true}
-        showsUserLocation={true}
+      <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map} zoomEnabled={true} showsUserLocation={true}
       />
       <TouchableOpacity activeOpacity={0.7} style={styles.buttonSync} onPress={syncLocations}>
         <Icon name="refresh-cw" size={24} color="#FFF"/>
